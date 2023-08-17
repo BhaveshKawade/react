19c61e8e565adb1cc003032a2b54ff3835853070
Bhavesh Kawade <bhaveshkawade@gmail.com>
2023-08-17 14:10:16 +0530

    JSX, React Component, jsx element, component composition

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("div",{id:"parent"},
-React.createElement("div", {id:"child1"}, 
-    React.createElement("h1", {id:"child1_h1"}, "This is child1 heading1"),
-    React.createElement("h2",{id:"child1_h2"},"This is child1 heading2"),    
-),
-React.createElement("div",{id:"child2"},
-    React.createElement("h1",{id:"child2_h1"},"This is child2 heading1"),
-    React.createElement("h2",{id:"child2_h2"},"This is child2 heading2"),    
-)
-);
+
+const elem = <span>React Element</span>;
+
+
+const HeadingComponent = () => (<div id="headingComp" className="headingComp">
+        React Component
+        </div>
+);        
+
+const title = (
+        <h1 id="heading" className="heading">
+        {elem}
+        React Element
+        <HeadingComponent/>
+        </h1>
+        );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(title);
